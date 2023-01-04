0f54e55cb9753347fc8f9eb1f2cf9f8498f663a6
parkyejin <uiop5809@naver.com>
2023-01-04 18:26:23 +0900

    Feat: NowPlaying 현재 상영중 목록 구현 & Style: 레이아웃

---
+++ src/features/movie/nowPlaying/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "./../../../components/Card/index";
+import { Base, Title } from "./style";
+import useNowPlayingMovie from "./../../../hooks/movie/useNowPlayingMovie";
+import Slider from "./../../../components/Slider/index";
+
+const NowPlayingSection: React.FC = () => {
+  const { data, isLoading } = useNowPlayingMovie();
+
+  const getYear = (date: string) => date.split("-")[0];
+
+  return (
+    <Base>
+      <Title>현재 상영중</Title>
+      {isLoading || !data ? (
+        <div> Loading... </div>
+      ) : (
+        <Slider>
+          {data.data.results.map((movie) => (
+            <Card
+              key={movie.id}
+              linkUrl={`/movie/${movie.id}`}
+              title={movie.title}
+              posterPath={`${process.env.REACT_APP_IMAGE_PREFIX}/${movie.poster_path}`}
+              voteAverage={movie.vote_average}
+              year={getYear(movie.release_date)}
+            />
+          ))}
+        </Slider>
+      )}
+    </Base>
+  );
+};
+
+export default NowPlayingSection;
--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from "../../components/Footer/index";
 import Header from "../../components/Header/index";
+import LatestMovieSection from "../../features/movie/latest";
+import NowPlayingSection from "../../features/movie/nowPlaying";
 
 function MainPage() {
   return (
     <div>
       <Header />
+      <LatestMovieSection />
+      <NowPlayingSection />
       <Footer />
     </div>
   );
