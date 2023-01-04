7fbb00b3426f439638c2e46aafb1f2c7bc319497
parkyejin <uiop5809@naver.com>
2023-01-04 15:51:51 +0900

    Feat: MainPage에 Footer 컴포넌트 배치

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./../components/Footer/index";
 import Header from "./../components/Header/index";
 
 function MainPage() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
