66f9063772909c34a44a2aee9a4a8adc9847db7c
parkyejin <uiop5809@naver.com>
2023-01-04 15:37:57 +0900

    Feat: MainPage에 Header 컴포넌트 배치

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./../components/Header/index";
 
 function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default MainPage;
