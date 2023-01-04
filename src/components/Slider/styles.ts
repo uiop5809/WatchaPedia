fce7a71e603669790346974c95760e19d2d57c30
parkyejin <uiop5809@naver.com>
2023-01-04 17:01:14 +0900

    Style: slider 레이아웃

---
+++ src/components/Slider/styles.ts
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+const ArrowButton = styled.button<{ pos?: "left" | "right" }>`
+  padding: 16px;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  z-index: 1;
+  top: 50%;
+  background-color: #fff;
+  ${({ pos }) =>
+    pos === "left"
+      ? css`
+          left: 0;
+          transform: translate(-50%, -50%);
+        `
+      : css`
+          right: 0;
+          transform: translate(50%, -50%);
+        `};
+  &:before {
+    content: initial;
+  }
+  > svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 10px;
+    height: 10px;
+    color: #222;
+  }
+`;
+
+export default ArrowButton;
